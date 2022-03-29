e6d7dd51ee36b25c4832d6296a5f00d30221c295
waiga <hyang9387@gmail.com>
2022-03-29 23:39:17 +0900

    Initial Commit

---
+++ proto_test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto_test1
+//
+//  Created by 김미향 on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
